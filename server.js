9a9c55d74ed114611cae3b755b96501fc1806435
s.binder <s.binder@dundeeandangus.ac.uk>
2018-03-03 09:34:23 +0000

    2.0 second page (w/ database connection info)

--- server.js
+++ server.js
@@ -77,8 +77,28 @@ app.get('/', function (req, res) {
   }
 });
 
-//app.get('/pagecount', function (req, res) {
 app.get('/second.html', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('second.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('second.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
